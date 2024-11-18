96b9c103b82d8e76064d48425861ad70018854fe
Mansadhit301 <aadhit407@gmail.com>
2024-11-19 00:05:48 +0800

    setup error handler

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ require("dotenv").config();
 
 const express = require("express");
 const multer = require("multer");
+const httpStatus = require("http-status");
+const helmet = require("helmet");
 const config = require("./src/utils/config");
 const routes = require("./src/routes/index");
+const { errorHandler } = require("./src/middlewares/error");
+const ApiError = require("./src/utils/ApiError")
 
 const app = express();
 
+// set security HTTP headers
+app.use(helmet());
+
 // parse json request body
 app.use(express.json());
+
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
@@ -18,9 +26,13 @@ app.use(upload.none());
 // API routes
 app.use(routes);
 
-app.get("/", (req, res) => {
-  res.send(`Hello World! Running in ${config.app.env} mode`);
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
+
+// internal server error handling
+app.use(errorHandler)
 
 const PORT = config.app.port || 3000;
 app.listen(PORT, config.app.host, () => {
---
+++ src/middlewares/error.js
@@ -0,0 +1,18 @@
+const httpStatus = require("http-status");
+const ApiError = require("../utils/ApiError");
+
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
+  let { statusCode, message } = err;
+  if (!(err instanceof ApiError)) {
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    message = "Internal Server Error";
+  }
+
+  res.status(statusCode).send({
+    status: statusCode,
+    message,
+  });
+};
+
+module.exports = { errorHandler };
