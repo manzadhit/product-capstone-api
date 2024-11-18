32001a3fd6a8f82d54774afa78deae4456073432
Mansadhit301 <aadhit407@gmail.com>
2024-11-18 21:40:58 +0800

    change port and host

--- app.js
+++ app.js
@@ -5,28 +5,35 @@ const multer = require("multer");
 const config = require("./src/utils/config");
 const routes = require("./src/routes/index");
 
-
 const app = express();
 const upload = multer();
 
-// parse json request body
+// Tentukan environment
+const ENV = process.env.NODE_ENV || "development";
+
+// Konfigurasi port berdasarkan environment
+const PORT =
+  ENV === "production"
+    ? process.env.PORT || 8080 // Production
+    : config.app.port || 3000; // Development
+
+// Konfigurasi host
+const HOST = ENV === "production" ? "0.0.0.0" : "localhost";
+
+// Middleware
 app.use(express.json());
 app.use(upload.none());
-// parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
-// API routes
+// Routes
 app.use(routes);
 
 app.get("/", (req, res) => {
   res.send("Hello World1");
 });
 
-const PORT = config.app.port || 3000;
-const ENV = process.env.NODE_ENV || "development";
 
-// Host berdasarkan environment
-const HOST = ENV === "production" ? "0.0.0.0" : "localhost";
+// Start server
 app.listen(PORT, HOST, () => {
-  console.log(`App listening on http://${HOST}:${config.app.port}`);
+  console.log(`App listening on http://${HOST}:${PORT}`);
 });
