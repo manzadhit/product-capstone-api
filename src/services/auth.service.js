2683e6770295f342dfabde1497de9c764f34410e
Mansadhit301 <aadhit407@gmail.com>
2024-11-25 20:45:13 +0800

    change message if email not registered

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,13 +6,26 @@ const { findUserByEmail } = require("./user.service");
 
 const loginUserWithEmailAndPassword = async (email, password) => {
   const user = await findUserByEmail(email);
+
+  if (!user) {
+    throw new ApiError(
+      httpStatus.NOT_FOUND,
+      "The email provided is incorrect or not registered."
+    );
+  }
+
   const validPassword = await bcrypt.compare(password, user.password);
 
-  if (!user || !validPassword) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, "Incorrect email or password");
+  if (!validPassword) {
+    throw new ApiError(
+      httpStatus.UNAUTHORIZED,
+      "The password you entered is incorrect."
+    );
   }
+
   return user;
 };
+
 
 module.exports = {
   loginUserWithEmailAndPassword,
