a8a2f30f3d18af37e2eff4e0b690994b73f7dca6
umar <c206b4ky4376@bangkit.academy>
2024-11-26 21:59:10 +0800

    memperbaiki dir config pada file passposrt js

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,8 +1,36 @@
+// passport.js
 require("dotenv").config();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
 const { userService } = require("../services");
 const config = require("../config/config");
 
+// Konfigurasi untuk Google OAuth
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // Simpan atau verifikasi data pengguna di database
+        const user = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          email: profile.emails[0].value,
+        };
+        return done(null, user);
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+// Konfigurasi untuk JWT (Json Web Token)
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: config.jwt.secret,
@@ -17,4 +45,7 @@ const jwtVerify = async (payload, done) => {
 
 const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
 
-module.exports = jwtStrategy;
+// Menggunakan JWT strategy
+passport.use(jwtStrategy);
+
+module.exports = passport;
