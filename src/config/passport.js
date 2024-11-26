0b3ab4614dc6d0c117292b98c95fe63daed61375
umar <c206b4ky4376@bangkit.academy>
2024-11-26 21:24:39 +0800

    perbaikan di dir config , menjadikan satu di file passportjs

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,20 +1,52 @@
 require("dotenv").config();
+const passport = require("passport");
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const { userService } = require("../services");
 const config = require("../config/config");
 
+// JWT Strategy
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: config.jwt.secret,
 };
 
 const jwtVerify = async (payload, done) => {
-  const user = await userService.getUserById(payload.id);
-
-  if (user) return done(null, user);
-  return done(null, false);
+  try {
+    const user = await userService.getUserById(payload.id);
+    if (user) {
+      return done(null, user);
+    }
+    return done(null, false);
+  } catch (error) {
+    return done(error, false);
+  }
 };
 
-const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
+passport.use("jwt", new JwtStrategy(jwtOptions, jwtVerify));
 
-module.exports = jwtStrategy;
+// Google OAuth Strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_REDIRECT_URI,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // Simpan atau verifikasi data pengguna di database
+        const user = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          email: profile.emails[0].value,
+        };
+        return done(null, user);
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+module.exports = passport;
