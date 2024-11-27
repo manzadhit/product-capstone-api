6e5fad7f94a94c204ac9f662d75e486c7adc82c4
Mansadhit301 <aadhit407@gmail.com>
2024-11-27 14:44:38 +0800

    change done in googleVerify

--- src/config/passport.js
+++ src/config/passport.js
@@ -23,22 +23,19 @@ const googleOption = {
 };
 
 const googleVerify = async (profile, done) => {
-  try {
-    let user = await userService.findUserByGoogleId(profile.id);
+  let user = await userService.findUserByGoogleId(profile.id);
 
-    // Jika tidak ditemukan, buat user baru dengan role "user"
-    if (!user) {
-      user = await user.createUser({
-        googleId: profile.id,
-        username: profile.displayName,
-        email: profile.emails[0].value,
-        avatar: profile.photos?.[0]?.value || null,
-      });
-    }
-    return done(null, user);
-  } catch (err) {
-    return done(err, null);
+  // Jika tidak ditemukan, buat user baru dengan role "user"
+  if (!user) {
+    user = await user.createUser({
+      googleId: profile.id,
+      username: profile.displayName,
+      email: profile.emails[0].value,
+      avatar: profile.photos?.[0]?.value || null,
+    });
   }
+  if (user) return done(null, user);
+  return done(null, false);
 };
 
 const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
