fd843e2528604758fb81e2af26c76b95aa073419
Mansadhit301 <aadhit407@gmail.com>
2024-11-27 15:07:39 +0800

    use old callback in route google/callback

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,6 +4,8 @@ const authController = require("../controllers/auth.controller");
 const validate = require("../middlewares/validation");
 const { authValidation } = require("../validations");
 const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
 
 // Rute register
 router.post("/register", validate(authValidation.register), authController.register);
@@ -18,7 +20,28 @@ router.get(
 
 router.get(
   "/google/callback",
-  passport.authenticate("google", { failureRedirect: "/login" }, authController.callbackGoogle)
+  passport.authenticate("google", { failureRedirect: "/login" }, (req, res) => {
+    try {
+      // Generate token JWT untuk user
+      const token = jwt.sign(
+        {
+          id: req.user.googleId,
+          email: req.user.email,
+          name: req.user.displayName,
+        },
+        config.jwt.secret,
+        { expiresIn: "1h" }
+      );
+
+      const user = req.user;
+
+      // Kirim token JWT sebagai respons
+      res.status(200).json({ user, token });
+    } catch (error) {
+      // Gunakan objek error untuk memberikan informasi lebih detail
+      res.status(500).json({ error: error.message || "Internal Server Error" });
+    }
+  })
 );
 
 module.exports = router;
