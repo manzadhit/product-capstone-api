53d884338a557ccb0704a2673a89f74f37243cdb
Mansadhit301 <aadhit407@gmail.com>
2024-12-12 01:35:04 +0800

    add projectId to configurasi firebase

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -2,6 +2,7 @@ const admin = require("firebase-admin");
 const path = require("path");
 
 admin.initializeApp({
+  projectId: "nutricheck-441702",
   credential: admin.credential.cert(path.join(__dirname, "./key.json")),
 });
 
