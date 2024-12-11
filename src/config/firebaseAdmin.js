2c6d1ecdf02769b32eed9d992c6d84821e05e293
Mansadhit301 <aadhit407@gmail.com>
2024-12-12 01:42:33 +0800

    add different service account for firebase

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 admin.initializeApp({
   projectId: "nutricheck-441702",
-  credential: admin.credential.cert(path.join(__dirname, "./key.json")),
+  credential: admin.credential.cert(path.join(__dirname, "./firebaseKey.json")),
 });
 
 module.exports = admin ;
